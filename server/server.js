4d70c8b97b0f191cc816bc70ef187976bb0c3ef1
Katy <katy.ledgard@gmail.com>
2023-07-28 15:38:48 +0100

    remove unused code

--- server/server.js
+++ server/server.js
@@ -14,20 +14,7 @@ app.get("/", (request, response)=>{
 
 app.get("/animal", (request, response)=>{
  response.json(data)
-git})
+})
 
-// app.get("/habitat", async (request, response)=>{
-// const query = `http://localhost:8082/animal?query=${request.query.habitat}`;
-// const res = await axios.get(query);
-
-// const something = res.data.results.filter((habitat)=>{})
-
-
-//     const habitat = request.query.habitat;
-
-//     const animalObj = data.find((animal)=> animal.habitat == habitat)
-// response.json("help")
-// console.log(animalObj)
-// })
 
 app.listen(PORT, ()=>console.log(`app is listening on port ${PORT}`));
