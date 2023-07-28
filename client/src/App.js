b80e3483ee235d432b05b610f095487a04d5e984
Katy <katy.ledgard@gmail.com>
2023-07-28 14:43:40 +0100

    add feauture to display all data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,80 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Header from "./Components/Header/Header";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [allAnimals, setAllAnimals] = useState("");
+
+  function handleSearch(event) {
+    // console.log("working")
+    setSearchQuery(event.target.value);
+    // console.log(searchQuery)
+  }
+
+  async function getAnimals(event) {
+    const API = "http://localhost:8082/animal";
+    const res = await axios.get(API);
+    // const animals = res.map((animal)=> {
+    // return (
+    //   animal.name
+    // )}
+    // )
+    console.log(res.data);
+
+    setAllAnimals(res.data);
+  }
+
+  function hideAnimals(event) {
+    setAllAnimals(!allAnimals);
+  }
+
+  // function displayAnimals(){
+  //   allAnimals.map((animal)=>{
+  //     return
+  //         <p>{animal.name}</p>
+  //         // imgUrl:animal.image_url,
+  //         // description:animal.description,
+  //         // species:animal.species,
+  //         // habitat:animal.habitat,
+
+  //   })
+  // }
+
   return (
     <div className="App">
-      yo
-        
+      <Header />
+      <button className="get-btn" onClick={getAnimals}>
+        Get all animals
+      </button>
+      {allAnimals && (
+        <button onClick={hideAnimals} className="hide-btn">
+          Hide Animals
+        </button>
+      )}
+      {allAnimals &&
+        allAnimals.map((animal) => {
+          return (
+            <div className="animal-container">
+              <h2>{animal.name}</h2>
+              <img src={animal.image_url} alt={animal.name} />
+              <p>{animal.description}</p>
+              <p>
+                <span className="bold">Species:</span> {animal.species}
+              </p>
+              <p>
+                <span className="bold">Habitat: </span>
+                {animal.habitat}
+              </p>
+            </div>
+          );
+        })}
+
+      <h2>Find an animal by Habitat</h2>
+
+      <input type="text" placeholder="search habitat" onChange={handleSearch} />
+      {/* <button onClick={getImage}>Explore!</button> */}
     </div>
   );
 }
